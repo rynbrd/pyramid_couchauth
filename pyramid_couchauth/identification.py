cb2960619db8da6c0b4859c4012cf101c9e58a19
Ryan Bourgeois <bluedragonx@gmail.com>
2012-07-30 11:38:18 -0500

    Adjusted whitespace, added missing docstrings, fixed spelling of principal, and moved interface usage to a class decorator.

--- pyramid_couchauth/identification.py
+++ pyramid_couchauth/identification.py
@@ -8,21 +8,26 @@
 # This software is provided "as is" and any express or implied warranties,
 # including, but not limited to, the implied warranties of merchantability and
 # fitness for a particular purpose are disclaimed.
+"""
+Identification implementations.
+"""
 
-from zope.interface import implements
+from zope.interface import implementer
 from pyramid.authentication import AuthTktCookieHelper
 from pyramid_couchauth.interfaces import IIdentifier
 
+
+@implementer(IIdentifier)
 class AuthTktIdentifier:
+
     """
-    An identifier for storing the currently authenticated principle in an
+    An identifier for storing the currently authenticated principal in an
     authtkt cookie. Uses pyramid.authentication.AuthTktCookieHelper.
     """
-    implements(IIdentifier)
 
     def __init__(self, secret, cookie_name='auth_tkt', secure=False,
-            include_ip=False, timeout=None, reissue_time=None, max_age=None,
-            path="/", http_only=False, wild_domain=True):
+        include_ip=False, timeout=None, reissue_time=None, max_age=None,
+        path="/", http_only=False, wild_domain=True):
         """
         Initialize the identifier. Takes the same arguments as
         pyramid.authentication.AuthTktCookieHelper.
@@ -35,6 +40,7 @@ class AuthTktIdentifier:
     def identity(self, request):
         """
         Return the username of the remembered user.
+
         :param request: The WSGI request.
         :return: The username of the remembered user.
         """
@@ -43,7 +49,8 @@ class AuthTktIdentifier:
 
     def remember(self, request, username, **kw):
         """
-        Return the headers necessary for remembering the principle.
+        Return the headers necessary for remembering the principal.
+
         :param request: The WSGI request.
         :param username: The username to remember.
         :param kw: Additional identifier parameters.
@@ -53,7 +60,8 @@ class AuthTktIdentifier:
 
     def forget(self, request):
         """
-        Return the headers necessary for forgetting any remembered principle.
+        Return the headers necessary for forgetting any remembered principal.
+
         :param request: The WSGI request.
         :return: A list of headers to add to the response.
         """
