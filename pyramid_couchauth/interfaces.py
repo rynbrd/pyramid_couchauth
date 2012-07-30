57e78c10a95a77977cb47434b454f42da032cc24
Ryan Bourgeois <bluedragonx@gmail.com>
2012-07-30 11:41:33 -0500

    Adjusted whitespace and added missing docstrings.

--- pyramid_couchauth/interfaces.py
+++ pyramid_couchauth/interfaces.py
@@ -8,10 +8,16 @@
 # This software is provided "as is" and any express or implied warranties,
 # including, but not limited to, the implied warranties of merchantability and
 # fitness for a particular purpose are disclaimed.
+"""
+Interfaces for use when implementing portions of pyramid_couchauth in a
+project.
+"""
 
 from zope.interface import Interface
 
+
 class IIdentifier(Interface):
+
     """
     Interface for identifying and saving a currently authenticated principal.
     """
@@ -20,16 +26,18 @@ class IIdentifier(Interface):
         """
         Return the username of the currently remembered user or None if no user
         has been remembered.
+
         :param request: The WSGI request.
         :return: The remembered username or None.
         """
 
-    def remember(self, request, username, **kw):
+    def remember(self, request, principal, **kw):
         """
         Return a list of headers necessary for remembering the given username.
         These will be passed as part of the WSGI response.
+
         :param request: The WSGI request.
-        :param principle: The principle to save.
+        :param principal: The principal to save.
         :param kw: Additional identifier parameters.
         :return: A list of headers to add to the response.
         """
@@ -38,6 +46,7 @@ class IIdentifier(Interface):
         """
         Return a list of headers necessary for forgetting the remembered
         username.
+
         :param request: The WSGI request.
         :return: A list of headers to add to the response.
         """
