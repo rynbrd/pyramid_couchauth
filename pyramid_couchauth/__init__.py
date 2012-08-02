9cde648346771ecc5c16325863c733ff7dd08ab8
Ryan Bourgeois <bluedragonx@gmail.com>
2012-08-01 22:23:24 -0500

    Added configure function to simplify setting up the auth/auth policies.

--- pyramid_couchauth/__init__.py
+++ pyramid_couchauth/__init__.py
@@ -12,3 +12,34 @@
 Implements auth/auth support in Pyramid against CouchDB.
 """
 
+def configure(config, database):
+    """
+    Load Pyramid with the couchauth auth/auth policies.
+
+    Settings:
+      couchauth.secret -- The shared secret used by the AuthTkt identifier.
+
+    :param config: The Pyramid config object.
+    :param database: The couchdbkit database containing the authentication
+        views.
+    """
+    settings = config.get_settings()
+
+    def get_setting(name, default=None):
+        if name in settings:
+            return settings[name]
+        else:
+            return default
+
+    from pyramid_couchauth.identification import AuthTktIdentifier
+    from pyramid_couchauth.policies import (CouchAuthenticationPolicy,
+        CouchAuthorizationPolicy)
+
+    secret = get_setting('couchauth.secret', 'secret'))
+    identifier = AuthTktIdentifier(secret)
+    authentication = CouchAuthenticationPolicy(database, identifier)
+    authorization = CouchAuthorizationPolicy(database)
+
+    config.set_authentication_policy(authentication)
+    config.set_authorization_policy(authorization)
+
