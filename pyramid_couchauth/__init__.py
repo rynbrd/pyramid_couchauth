e8aed5c35ecbbe78c9a4ea52c7f988fa5b98d2fe
Ryan Bourgeois <bluedragonx@gmail.com>
2012-08-07 15:22:09 -0500

    Revert "Updated configure function to store modeul global session object. Added test coverage for __init__."
    This reverts commit 69072834ba2b12eecd02bfaa5e09fb28375ba361.

--- pyramid_couchauth/__init__.py
+++ pyramid_couchauth/__init__.py
@@ -7,87 +7,33 @@ Implements auth/auth support in Pyramid against CouchDB.
 """
 
 
-class Session:
-
+def configure(config, database):
     """
-    Store session information.
-    """
+    Load Pyramid with the couchauth auth/auth policies.
 
-    db_key = 'couchauth.db'
-    db_default = 'auth'
-    secret_key = 'couchauth.secret'
-    secret_default = 'secret'
+    Settings:
+      couchauth.secret -- The shared secret used by the AuthTkt identifier.
 
-    def __init__(self):
-        """
-        Initialize the object.
-        """
-        self.configured = False
-        self.secret = None
-        self.dbname = None
-        self._database = None
-
-    def configure(self, settings):
-        """
-        Configure the session. Attempts to retrieve the database name from
-        couchauth.db and the shared secret from couchauth.secret.
-        """
-        self.dbname = self.db_default
-        if self.db_key in settings:
-            self.dbname = settings[self.db_key]
-        self._database = None
-
-        if self.secret_key in settings:
-            self.secret = settings[self.secret_key]
-        else:
-            self.secret = self.secret_default
-
-        self.configured = True
-
-    @property
-    def database(self):
-        """The auth/auth database."""
-        if self._database is None:
-            import pyramid_couchdb
-            self._database = pyramid_couchdb.session.server.get_db(self.dbname)
-        return self._database
-
-    @database.setter
-    def database(self, database):
-        """The auth/auth database."""
-        self._database = database
-
-
-session = Session()
-
-
-def configure(config, secret=None, database=None):
-    """
-    Configure Pyramid to use couchauth.
-
-    :param config: The pyramid config object.
-    :param secret: The shared secret. If not None it overrides the value in the
-        session object.
-    :param database: The auth/auth database. If not None it overrides the value
-        in the session object.
+    :param config: The Pyramid config object.
+    :param database: The couchdbkit database containing the authentication
+        views.
     """
     settings = config.get_settings()
-    session.configure(settings)
-    if secret is not None:
-        session.secret = secret
-    if database is not None:
-        if isinstance(database, basestring):
-            import pyramid_couchdb
-            database = pyramid_couchdb.session.server.get_db(database)
-        session.database = database
+
+    def get_setting(name, default=None):
+        if name in settings:
+            return settings[name]
+        else:
+            return default
 
     from pyramid_couchauth.identification import AuthTktIdentifier
     from pyramid_couchauth.policies import (CouchAuthenticationPolicy,
         CouchAuthorizationPolicy)
 
-    identifier = AuthTktIdentifier(session.secret)
-    authentication = CouchAuthenticationPolicy(session.database, identifier)
-    authorization = CouchAuthorizationPolicy(session.database)
+    secret = get_setting('couchauth.secret', 'secret')
+    identifier = AuthTktIdentifier(secret)
+    authentication = CouchAuthenticationPolicy(database, identifier)
+    authorization = CouchAuthorizationPolicy(database)
 
     config.set_authentication_policy(authentication)
     config.set_authorization_policy(authorization)
