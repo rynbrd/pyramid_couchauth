ddcbe8ae52c4d72d30284bc487b446294c366275
Ryan Bourgeois <bluedragonx@gmail.com>
2012-08-07 15:28:48 -0500

    Added view names as parameters to configure.

--- pyramid_couchauth/__init__.py
+++ pyramid_couchauth/__init__.py
@@ -7,7 +7,10 @@ Implements auth/auth support in Pyramid against CouchDB.
 """
 
 
-def configure(config, database):
+def configure(config, database user_names_view='pyramid/user_names',
+        user_groups_view='pyramid/user_groups', user_perms_view=None,
+        group_perms_view='pyramid/group_perms', perm_users_view=None,
+        perm_groups_view='pyramid/perm_groups'):
     """
     Load Pyramid with the couchauth auth/auth policies.
 
@@ -17,6 +20,29 @@ def configure(config, database):
     :param config: The Pyramid config object.
     :param database: The couchdbkit database containing the authentication
         views.
+    :param user_names_view: A view which maps the username as the key. The
+        value may be anything as this view is only used to validate the
+        existance of a user. Defaults to 'pyramid/user_names'.
+    :param user_groups_view: A view which maps group names (the value) to
+        their member usernames (the key). This view is used to expand a
+        user principal into group principals. Defaults to
+        'pyramid/user_groups'.
+    :param user_perms_view: A view which maps permission names (the values)
+        to usernames (the keys). A None value disables direct user
+        permission mapping. This is useful when using groups for all
+        permission controls. Defaults to None.
+    :param group_perms_view: A view which maps permission names (the
+        values) to group names (the keys). A None value disables group
+        permission mapping. This is useful if you wish all permissions to
+        be controlled at the user level. Defaults to 'pyramid/group_perms'.
+    :param perm_users_view: A view which maps usernames (the values) to
+        permission names (the keys). A None value disables permission user
+        mapping. This is useful when using groups for all permission
+        controls.  Defaults to None.
+    :param perm_groups_view: A view which maps group names (the values) to
+        permission names (the keys). A None value disables permission group
+        mapping. This is useful if you wish all permissions to be
+        controlled at the user level. Defaults to 'pyramid/perm_groups'.
     """
     settings = config.get_settings()
 
