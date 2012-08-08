0ca129d335a894dfbf0265e3b29e0dde7ccd6f3f
Ryan Bourgeois <bluedragonx@gmail.com>
2012-08-07 22:27:28 -0500

    Fixed total git revert screwup.

--- pyramid_couchauth/__init__.py
+++ pyramid_couchauth/__init__.py
@@ -7,19 +7,72 @@ Implements auth/auth support in Pyramid against CouchDB.
 """
 
 
-def configure(config, database user_names_view='pyramid/user_names',
+class Session:
+
+    """
+    Store session information.
+    """
+
+    db_key = 'couchauth.db'
+    db_default = 'auth'
+    secret_key = 'couchauth.secret'
+    secret_default = 'secret'
+
+    def __init__(self):
+        """
+        Initialize the object.
+        """
+        self.configured = False
+        self.secret = None
+        self.dbname = None
+        self._database = None
+
+    def configure(self, settings):
+        """
+        Configure the session. Attempts to retrieve the database name from
+        couchauth.db and the shared secret from couchauth.secret.
+        """
+        self.dbname = self.db_default
+        if self.db_key in settings:
+            self.dbname = settings[self.db_key]
+        self._database = None
+
+        if self.secret_key in settings:
+            self.secret = settings[self.secret_key]
+        else:
+            self.secret = self.secret_default
+
+        self.configured = True
+
+    @property
+    def database(self):
+        """The auth/auth database."""
+        if self._database is None:
+            import pyramid_couchdb
+            self._database = pyramid_couchdb.session.server.get_db(self.dbname)
+        return self._database
+
+    @database.setter
+    def database(self, database):
+        """The auth/auth database."""
+        self._database = database
+
+
+session = Session()
+
+
+def configure(config, secret=None, database=None, user_names_view='pyramid/user_names',
         user_groups_view='pyramid/user_groups', user_perms_view=None,
         group_perms_view='pyramid/group_perms', perm_users_view=None,
         perm_groups_view='pyramid/perm_groups'):
     """
-    Load Pyramid with the couchauth auth/auth policies.
+    Configure Pyramid to use couchauth.
 
-    Settings:
-      couchauth.secret -- The shared secret used by the AuthTkt identifier.
-
-    :param config: The Pyramid config object.
-    :param database: The couchdbkit database containing the authentication
-        views.
+    :param config: The pyramid config object.
+    :param secret: The shared secret. If not None it overrides the value in the
+        session object.
+    :param database: The auth/auth database. If not None it overrides the value
+        in the session object.
     :param user_names_view: A view which maps the username as the key. The
         value may be anything as this view is only used to validate the
         existance of a user. Defaults to 'pyramid/user_names'.
@@ -45,22 +98,23 @@ def configure(config, database user_names_view='pyramid/user_names',
         controlled at the user level. Defaults to 'pyramid/perm_groups'.
     """
     settings = config.get_settings()
-
-    def get_setting(name, default=None):
-        if name in settings:
-            return settings[name]
-        else:
-            return default
+    session.configure(settings)
+    if secret is not None:
+        session.secret = secret
+    if database is not None:
+        if isinstance(database, basestring):
+            import pyramid_couchdb
+            database = pyramid_couchdb.session.server.get_db(database)
+        session.database = database
 
     from pyramid_couchauth.identification import AuthTktIdentifier
     from pyramid_couchauth.policies import (CouchAuthenticationPolicy,
         CouchAuthorizationPolicy)
 
-    secret = get_setting('couchauth.secret', 'secret')
-    identifier = AuthTktIdentifier(secret)
-    authentication = CouchAuthenticationPolicy(database, identifier,
+    identifier = AuthTktIdentifier(session.secret)
+    authentication = CouchAuthenticationPolicy(session.database, identifier,
         user_names_view, user_groups_view)
-    authorization = CouchAuthorizationPolicy(database, user_perms_view,
+    authorization = CouchAuthorizationPolicy(session.databaseuser_perms_view,
         group_perms_view, perm_users_view, perm_groups_view)
 
     config.set_authentication_policy(authentication)
